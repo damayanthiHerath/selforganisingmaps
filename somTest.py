151497f78545d1fd088d6bf91b5d8b8eefea0600
damayanthiHerath <damayanthikherath@gmail.com>
2015-12-22 11:40:23 +1100

    Adding test file for som
    Test file for SOM

---
+++ somTest.py
@@ -0,0 +1,23 @@
+from som import som
+import numpy as numpy
+input = numpy.array(
+         [[1., 0., 0.],
+          [1., 0., 1.],
+          [0., 0., 0.5],
+          [0.125, 0.529, 1.0],
+          [0.33, 0.4, 0.67],
+          [0.6, 0.5, 1.0],
+          [0., 1., 0.],
+          [1., 0., 0.],
+          [0., 1., 1.],
+          [1., 0., 1.],
+          [1., 1., 0.],
+          [1., 1., 1.],
+          [.33, .33, .33],
+          [.5, .5, .5],
+          [.66, .66, .66]])
+somCol = 3
+somRow = 3
+som = som(input,12,somCol,somRow)
+ans =som.trainmodel()
+print 'trained model is',ans
